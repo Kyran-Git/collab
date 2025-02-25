c43735515277afa2c31be46974beca42795b007e
Kyran-Git <niklauscareer@gmail.com>
2025-02-26 00:12:39 +0800

    Add like button functionality to Card component

--- projectSean/react-project/src/App.jsx
+++ projectSean/react-project/src/App.jsx
@@ -1,6 +1,12 @@
 import './App.css'
+import { useState } from 'react';
+
 
 const App = () => {
+
+  const [hasLiked, setHasLiked] = useState(false);
+  // const [variable, variableSetter] = useState();
+
   return (
     <div className='card-container'>
       <Card title="React" />
@@ -14,6 +20,11 @@ const Card = ( { title } ) => {
   return (
     <div className="card">
       <h1>{ title }</h1>
+
+      <button onClick={() => setHasLiked(true)}>
+        Like
+      </button>
+      
     </div>
   )
 }
